8643b637168c4a8bed30eb6348533a3c650ac001
SUCHAIz <sinthwannthkeiyrtiphumi@gmail.com>
2025-04-28 21:14:30 +0700

    Card
    SO Learn
    SO

---
+++ Assets/SO Learn/SO_Card.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu (menuName = "Profile/Card")]
+public class SO_Card : ScriptableObject
+{
+    [Header("Character Infos")]
+    public string Name;
+    public string Description;
+    public Type ElementalType;
+
+    [Space, ShowAssetPreview]
+    public Sprite CharacterImage;
+
+    [Header("Character Stats")]
+    public int Cost;
+    public int Health;
+    public int ATK;
+
+}
+
+public enum Type
+{
+    Fire,
+    Ice,
+    Water,
+    Lightning,
+    Physical,
+    Quantum,
+    Wind,
+}
